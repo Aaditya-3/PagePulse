189dc9f0ade50712a861de3fcb0e5f3824f30934
aaditya <aadityakothari333@gmail.com>
2026-07-24 23:52:25 +0530

    deplyed

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-    ],
+    "http://localhost:5173",
+    "https://pagepulse-4bgq.onrender.com/",
+ ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
